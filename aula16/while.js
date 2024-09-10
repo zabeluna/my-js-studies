bbb273367dccfc5d132bbf55bc2f0f00bdd7b6c6
izabelle <personalife503@gmail.com>
2024-09-10 11:17:51 -0300

    feat: studies

---
+++ aula16/while.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+
+    if (i < 10) {
+    console.log("True");
+ } 
+
+    else if (i > 10) {
+        console.log("False");
+    }
+}
---
+++ exercicios-finais/maxnumber.js
@@ -0,0 +1,9 @@
+function max(x, y) {
+    return x > y ? x : y;
+}
+
+const max2 = (x, y) => {
+    return x > y ? x : y; //seria como um if e else
+}
+
+console.log(max2(2, 4));
