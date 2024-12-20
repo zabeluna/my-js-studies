2d9918081584eb0daf6d7e819c318db0a1247346
izabelle <personalife503@gmail.com>
2024-12-20 07:16:59 -0300

    study: add ...[] (array)

---
+++ arrays/concatenando-arrays.js
@@ -0,0 +1,8 @@
+// [] = array ||| {} = objeto
+
+// for concatenation of arrays, i can use two methods: "...[]" or "name-of-array.concat(array)"
+
+const array1 = ['ana', 'maria', 'caio'];
+const array2 = ['matt', 'ray', 'oceane'];
+const arrayConcatenando = [...array1, ...array2];
+console.log(arrayConcatenando);
